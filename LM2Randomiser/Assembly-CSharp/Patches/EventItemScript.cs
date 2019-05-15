34795708dce6c488294d956bb8124399de2b18ef
Coookie93 <bencook206@gmail.com>
2019-05-15 17:18:29 +0100

    changes to allow mantra randomisation

---
+++ LM2Randomiser/Assembly-CSharp/Patches/EventItemScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MonoMod;
+using L2Base;
+
+namespace LM2RandomiserMod.Patches
+{
+    [MonoModPatch("global::EventItemScript")]
+    public class patched_EventItemScript : global::EventItemScript
+    {
+        [MonoModReplace]
+        protected override void itemGetAction()
+        {
+            int slotNo = base.getL2Core().seManager.playSE(null, 39);
+            base.getL2Core().seManager.releaseGameObjectFromPlayer(slotNo);
+            this.pl.setActionOder(PLAYERACTIONODER.getitem);
+            this.pl.setGetItem(ref this.itemLabel);
+            if (this.itemLabel.Contains("Mantra") && !this.itemLabel.Equals("Mantra")) return;
+            this.pl.setGetItemIcon(L2SystemCore.getItemData(this.itemLabel));
+        }
+    }
+}
