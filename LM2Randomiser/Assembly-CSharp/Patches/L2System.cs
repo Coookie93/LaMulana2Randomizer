b4ef5e6ce7b68a7b9b63958510fd29c9d07b0f8f
Coookie93 <bencook206@gmail.com>
2019-05-15 17:22:17 +0100

    changes to allow mantra randomisation

--- LM2Randomiser/Assembly-CSharp/Patches/L2System.cs
+++ LM2Randomiser/Assembly-CSharp/Patches/L2System.cs
@@ -3,7 +3,7 @@ using MonoMod;
 using UnityEngine;
 using L2Word;
 
-#pragma warning disable 0649, 0414
+#pragma warning disable 0649, 0414, 0108
 namespace LM2RandomiserMod
 {
     [MonoModPatch("L2Base.L2System")]
@@ -23,6 +23,19 @@ namespace LM2RandomiserMod
             L2Rando component = obj.AddComponent<L2Rando>() as L2Rando;
             component.Initialise(this.l2sdb, this.l2tdb, this);
             DontDestroyOnLoad(obj);
+        }
+
+        public void orig_setItem(string item_name, int num, bool direct = false, bool loadcall = false, bool sub_add = true) { }
+        public void setItem(string item_name, int num, bool direct = false, bool loadcall = false, bool sub_add = true)
+        {
+            if (item_name.Contains("Mantra") && !name.Equals("Mantra"))
+            {
+                this.setFlagData(2, item_name, (short)(this.getItemNum(item_name) + num));
+            }
+            else
+            {
+                orig_setItem(item_name, num, direct, loadcall, sub_add);
+            }
         }
     }
 }
--- LM2Randomiser/Assembly-CSharp/Patches/ShopScript.cs
+++ LM2Randomiser/Assembly-CSharp/Patches/ShopScript.cs
@@ -27,55 +27,95 @@ namespace LM2RandomiserMod.Patches
         private int item_copunter;
 
         [MonoModIgnore]
+        private int[] item_num;
+
+        [MonoModIgnore]
+        private int[] item_value;
+
+        [MonoModIgnore]
         private Image[] shop_item;
 
         [MonoModIgnore]
-        private TextMeshProUGUI[] item_name;
+        private Text[] item_valu;
 
-        public extern bool orig_itemCallBack(string tab, string name, int vale, int num);
+        [MonoModIgnore]
+        private TextMeshProUGUI[] item_name;
+        
+        [MonoModReplace]
         public override bool itemCallBack(string tab, string name, int vale, int num)
         {
             string[] weapons = { "Whip2", "Whip3", "Knife", "Rapier", "Axe", "Katana", "Shuriken", "R-Shuriken", "E-Spear", "Flare Gun", "Bomb",
                                     "Chakram", "Caltrops", "Clay Doll", "Origin Seal", "Birth Seal", "Life Seal", "Death Seal"};
-
-            bool result = orig_itemCallBack(tab, name, vale, num);
-            if (result)
+            
+            if (this.item_copunter > 2)
             {
-                int counter = this.item_copunter - 1;
+                return false;
+            }
+            this.true_name[this.item_copunter] = name;
+            if (this.sys.isAnkJewel(name))
+            {
+                name = "Ankh Jewel";
+            }
+            else if (this.sys.isMap(name))
+            {
+                name = "Map";
+            }
+            this.item_id[this.item_copunter] = name;
+            
+            if (name.Contains("Mantra") && !name.Equals("Mantra"))
+            {
+                this.icon[this.item_copunter] = L2SystemCore.getShopIconSprite(L2SystemCore.getItemData("Mantra"));
+                this.shop_item[this.item_copunter].sprite = this.icon[this.item_copunter];
+
+                string mojiName = name.Equals("Mantra10") ? "mantra1stM10" : "mantra1stM" + name.Substring(6, 1);
+                this.item_name[this.item_copunter].text = this.sys.getMojiText(true, this.sys.mojiSheetNameToNo(tab, this.sys.getMojiScript(mojiScriptType.system)),
+                    this.sys.mojiIdToNo(tab, mojiName, this.sys.getMojiScript(mojiScriptType.system)), this.sys.getNowLangage(), this.sys.getMojiScript(mojiScriptType.system));
+            }
+            else
+            {
                 if (this.sys.isMap(name))
                 {
-                    this.icon[counter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData("Map"));
-                    this.shop_item[counter].sprite = this.icon[counter];
+                    this.icon[this.item_copunter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData("Map"));
                 }
                 else if (name.Equals("MSX"))
                 {
-                    this.icon[counter] = L2SystemCore.getShopIconSprite(L2SystemCore.getItemData("MSX3p"));
-                    this.shop_item[counter].sprite = this.icon[counter];
+                    this.icon[this.item_copunter] = L2SystemCore.getShopIconSprite(L2SystemCore.getItemData("MSX3p"));
                 }
                 else if (name.Contains("Sacred Orb"))
                 {
-                    this.icon[counter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData("Sacred Orb"));
-                    this.shop_item[counter].sprite = this.icon[counter];
-                    this.item_name[counter].text = this.sys.getMojiText(true, this.sys.mojiSheetNameToNo(tab, this.sys.getMojiScript(mojiScriptType.item)),
-                        this.sys.mojiIdToNo(tab, "Sacred Orb", this.sys.getMojiScript(mojiScriptType.item)), this.sys.getNowLangage(), this.sys.getMojiScript(mojiScriptType.item));
+                    this.icon[this.item_copunter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData("Sacred Orb"));
+                    name = "Sacred Orb";
                 }
                 else if (name.Contains("Crystal S"))
                 {
-                    this.icon[counter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData("Crystal S"));
-                    this.shop_item[counter].sprite = this.icon[counter];
-                    this.item_name[counter].text = this.sys.getMojiText(true, this.sys.mojiSheetNameToNo(tab, this.sys.getMojiScript(mojiScriptType.item)),
-                        this.sys.mojiIdToNo(tab, "Crystal S", this.sys.getMojiScript(mojiScriptType.item)), this.sys.getNowLangage(), this.sys.getMojiScript(mojiScriptType.item));
+                    this.icon[this.item_copunter] = L2SystemCore.getShopIconSprite(L2SystemCore.getItemData("Crystal S"));
+                    name = "Crystal S";
                 }
-                else
+                else if (Array.IndexOf(weapons, name) > -1)
                 {
-                    if (Array.IndexOf(weapons, name) > -1)
-                    {
-                        this.icon[counter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData(name));
-                        this.shop_item[counter].sprite = this.icon[counter];
-                    }
+                    this.icon[this.item_copunter] = L2SystemCore.getMenuIconSprite(L2SystemCore.getItemData(name));
                 }
+                else {
+                    this.icon[this.item_copunter] = ShopScript.Load("Textures/icons_shops", name);
+                }
+
+                this.shop_item[this.item_copunter].sprite = this.icon[this.item_copunter];
+                this.item_name[this.item_copunter].text = this.sys.getMojiText(true, this.sys.mojiSheetNameToNo(tab, this.sys.getMojiScript(mojiScriptType.item)),
+                        this.sys.mojiIdToNo(tab, name, this.sys.getMojiScript(mojiScriptType.item)), this.sys.getNowLangage(), this.sys.getMojiScript(mojiScriptType.item));
             }
-            return result;
+            
+            this.item_value[this.item_copunter] = vale;
+            if (vale > 999)
+            {
+                this.item_valu[this.item_copunter].text = L2Math.numToText(vale, 4);
+            }
+            else
+            {
+                this.item_valu[this.item_copunter].text = L2Math.numToText(vale, 3);
+            }
+            this.item_num[this.item_copunter] = num;
+            this.item_copunter++;
+            return true;
         }
 
         public void orig_setSouldOut() { }
