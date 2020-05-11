568c02a1c2b61f7d8a2af7e13c1703a386b5489a
Coookie93 <bencook206@gmail.com>
2020-05-11 14:46:15 +0100

    pricing changes

--- LaMulana2Randomizer/Item.cs
+++ LaMulana2Randomizer/Item.cs
@@ -1,22 +1,30 @@
-﻿using LaMulana2RandomizerShared;
+﻿using System;
 using Newtonsoft.Json;
+using LaMulana2RandomizerShared;
 
 namespace LaMulana2Randomizer
 {
     public class Item
     {
         public string Name { get; private set; }
+        public int Price { get; private set; }
         public bool IsRequired { get; private set; }
 
         [JsonIgnore]
         public ItemID ID { get; private set; }
 
         [JsonConstructor]
-        public Item(string name, ItemID id, bool isRequired = true)
+        public Item(string name, ItemID id, int price, bool isRequired = true)
         {
             Name = name;
             ID = id;
+            Price = price;
             IsRequired = isRequired;
+        }
+
+        public void AdjustPrice(float multiplier)
+        {
+            Price = (int)Math.Round(Price * multiplier);
         }
     }
 }
